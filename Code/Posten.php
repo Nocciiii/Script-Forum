c511d37a07c1c82d68f860e4f9ab2fb60adaae05
KuemmertJamie <47415865+KuemmertJamie@users.noreply.github.com>
2019-05-14 10:39:24 +0200

    Hinzufügen der Postfunktion für Kommentare

--- Code/Posten.php
+++ Code/Posten.php
@@ -27,6 +27,45 @@ function posten($nutzerID, $Ueberschrift, $Text)
         )"
       );
 }
+function kommentarPosten($nutzerID, $Text, $postID)
+{
+	$Veroeffentlichung=new DateTime;
+	$pdo->query("INSERT INTO
+        kommentar
+        (Inhalt,Veroeffentlichung)
+        VALUES
+        (
+          '.$Textt',
+          '.$Veroeffentlichung'
+        )"
+      );
+      $pdo->query("INSERT INTO
+        nutzer_kommentar
+        (NutzerID, KommentarID)
+        VALUES
+        (
+          '$nutzerID',
+          (SELECT k.ID
+           FROM Kommentar k
+           WHERE k.Inhalt ='.$text'
+           AND k.Veroeffentlichung='.$Veroeffentlichung'
+          )
+        )"
+      );
+      $pdo->query("INSERT INTO
+        post_kommentar
+        (PostID, KommentarID)
+        VALUES
+        (
+          '$postID',
+          (SELECT k.ID
+           FROM Kommentar k
+           WHERE k.Inhalt ='.$text'
+           AND k.Veroeffentlichung='.$Veroeffentlichung'
+          )
+        )"
+      );
+}
 
 
 ?>
