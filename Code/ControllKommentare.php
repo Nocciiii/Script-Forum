2547aa29093ab9274bb19b981bd5dff29a363848
FlohSchmidt <schmidtflorian@dv-schulen.de>
2019-05-29 14:18:01 +0200

    einfügen einer doku

--- Code/ControllKommentare.php
+++ Code/ControllKommentare.php
@@ -3,6 +3,7 @@
   {
     if(isset($_GET['post'])) //Checks if "book" is in the URL parameters
     {
+      //$selectedKommentar ist die ID des ausgewählten posts
       $selectedKommentar = $_GET['post'];
 
       //todo abgleichen der Datenbank mit dem Post
