056d960d7b3be240b1d4a923261e6457d7482e9c
KuemmertJamie <47415865+KuemmertJamie@users.noreply.github.com>
2019-06-04 09:23:19 +0200

    Abändern von Datenbankauslesen

--- Code/Startseite_Datenbankauslesen.php
+++ Code/Startseite_Datenbankauslesen.php
@@ -3,9 +3,15 @@
     // session isn't started
     session_start();
 }
+  $tabellenstring="<tr>";
   include("klassen.php");
-function Auslesen()
-{
+  $post_array = array();
+  Textausgeben($post_array);
+  $tabellenstring=$tabellenstring."</tr>";
+  return $tabellenstring;
+  
+	function Auslesen()
+	{
 	  $server  ='mysql:dbname=fi2017_gruppe1_projekt_adelmann_kuemmert_schmidt;
 	  host=localhost';
 	  $user='root';
@@ -15,7 +21,6 @@ function Auslesen()
 	  {
 	    $pdo = new PDO ($server, $user,'',$options);
 	    global $post_array;
-		  $post_array=array();
 	    foreach($pdo->query('SELECT * from post') as $row)
 	    {
 	      $post = new Post();
@@ -54,69 +59,48 @@ function Auslesen()
 }
 
   //Senden der Posts in forgegebener form an index zur ausgabe
-  function Textausgeben()
+  function Textausgeben($post_array)
   {
-		Auslesen();
+	Auslesen();
     foreach ($post_array as $key => $post)
     {
-	?>
-	 <script>
-      $("#komentar").detach();
-	 </script>
-	 <?php
-    }
-
-    foreach ($post_array as $key => $post)
-    {
-      if($_SESSION['Admin'] == true)
+      if($_SESSION['Admin'] == 1)
       {
-		?>
-		<script>
+		$tabellenstring=$tabellenstring."<td>";
         //auslesen der Überschrift des Posts sowie zuwiesung einer Post funktion als Link um die Kommentare zur seite anzuzeigen
-        var ueberschrift = "<a href='http://172.16.5.55/bsz/fi11_1/ ?post=".$post.getId()."'>
-          //Todo passender link einfügen!!!!!!!!!!!!!!!!!!!!!!
-                            <div class='col-md-12' id="komentar">
-                            <h1>" $post.getUeberschrift()"</h1>";
+        $ueberschrift = "<a href='http://172.16.5.55/bsz/fi11_1/ ?post=".$post.getId()."'>
+                            <div class='col-md-12' id='komentar'>
+                            <h1>' $post.getUeberschrift()'</h1>";
+		$tabellenstring=$tabellenstring.$ueberschrift;
         //auslesen des inhalts eines Posts
-        var inhalt = $post.getInhalt()"</div></a>";
-        var button = "<form action="postDelete($_SESSION['userid'], $post.getUeberschrift(), $post.getInhalt())">"
-                        "<input type="submit" value="Posten Deleten"></input>"
+        $inhalt = $post.getInhalt()."</div></a>";
+		$tabellenstring=$tabellenstring.$inhalt;
+        $button = "<form action='postDelete(".$_SESSION['userid'].",".$post.getUeberschrift().",".$post.getInhalt()."')'>".
+                        "<input type='submit' value='Posten Deleten'></input>".
                       "</form>";
-
-        $("#table").append(ueberschrift, inhalt, button);
-		</script>
-		<?php
+		$tabellenstring=$tabellenstring.$button;
+		$tabellenstring=$tabellenstring."</td>";
       }
       else
       {
-		?>
-		<script>
         //auslesen der Überschrift des Posts sowie zuwiesung einer Post funktion als Link um die Kommentare zur seite anzuzeigen
-        var ueberschrift = "<a href='http://172.16.5.55/bsz/fi11_1/ ?post=".$post.getId()."'>
-          //Todo passender link einfügen!!!!!!!!!!!!!!!!!!!!!!
-                            <div class='col-md-12' id="komentar">
-                            <h1>" $post.getUeberschrift()"</h1>";
+        $ueberschrift = "<a href='http://172.16.5.55/bsz/fi11_1/ ?post=".$post.getId()."'>
+                            <div class='col-md-12' id='komentar'>
+                            <h1>' $post.getUeberschrift()'</h1>";
+		$tabellenstring=$tabellenstring.$ueberschrift;
         //auslesen des inhalts eines Posts
-        var inhalt = $post.getInhalt()"</div></a>";
-
-        $("#table").append(ueberschrift, inhalt);
-		</script>
-		<?php
+        $inhalt = $post.getInhalt()."</div></a>";
+		$tabellenstring=$tabellenstring.$inhalt;
+		$tabellenstring=$tabellenstring."</td>";
       }
-		?>
-		<script>
+	  
+	  return $tabellenstring;
 //toDo Posts gewünscht sotieren
-      var uebershriftr = "<div class="col col-md-12">"$post.getUeberschrift()"</div>"
+      //$uebershriftr = "<div class='col col-md-12'>".$post.getUeberschrift()."</div>";
 
 //toDo selbes wie obendrüber
-      var uebershriftl = "<div class="col col-md-12">"$post.getUeberschrift()"</div>"
+      //$uebershriftl = "<div class='col col-md-12'>".$post.getUeberschrift()."</div>";
 
-      //Ausgabe der Überschrift und des Inhalts auf der Seite
-      $("#multiCollapseExample1").append(ueberschriftr);
-      $("#multiCollapseExample2").append(ueberschriftr);
-      $("#Collapsright").append(ueberschriftl);
-	  </script>
-	  <?php
     }
   }
 
