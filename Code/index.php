9e36ea9f20414f963d2f694560ed400efb546f98
FlohSchmidt <schmidtflorian@dv-schulen.de>
2019-06-05 14:03:39 +0200

    designen der Colapsbugen

--- Code/index.php
+++ Code/index.php
@@ -7,6 +7,7 @@
           <link rel="stylesheet" href="tutorial.css" />
   		<script src="../js/jquery-3.1.1.min.js"></script>
   		<script src="../js/bootstrap.min.js"></script>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
       <?php include ("Startseite_Datenbankauslesen.php"); ?>
       <?php include ("Posten.php"); ?>
 
@@ -316,7 +317,7 @@
 
         <div class="col-md-3 d-md-block">
           <button class="btn d-md-none" data-toggle="collapse" data-target=".multi-collapse" aria-expanded="false" aria-controls="multiCollapseExample1 multiCollapseExample2">
-            <span class="navbar-toggler-icon"></span>
+            <i class="fa fa-bars"></i>
           </button>
           <div class="row multi-collapse show" id="multiCollapseExample1">
           </div>
@@ -330,7 +331,7 @@
 
         <div class="col-md-3">
           <button class="btn d-md-none" data-toggle="collapse" data-target="#Collapsright" aria-expanded="false" aria-controls="collapseOne">
-            <span class="navbar-toggler-icon"></span>
+            <i class="fa fa-bars"></i>
           </button>
           <div class="row show" id="Collapsright">
             <div class="col-md-12 right">
